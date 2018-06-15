d4f687bae275075985ec4356824abb3334a61363
aust6512 <aust6512@gmail.com>
2018-06-15 15:59:42 -0500

    add mass units

--- source/PhysicalQuantity/Mass.php
+++ source/PhysicalQuantity/Mass.php
@@ -42,18 +42,52 @@ class Mass extends AbstractPhysicalQuantity
         $newUnit->addAlias('lbs');
         $newUnit->addAlias('pound');
         $newUnit->addAlias('pounds');
+        $newUnit->addAlias('9');
         static::addUnit($newUnit);
 
         // Ounce
         $newUnit = UnitOfMeasure::linearUnitFactory('oz', 4.5359237e-1 / 16);
         $newUnit->addAlias('ounce');
         $newUnit->addAlias('ounces');
+        $newUnit->addAlias('3');
         static::addUnit($newUnit);
 
         // Stone
         $newUnit = UnitOfMeasure::linearUnitFactory('st', 4.5359237e-1 * 14);
         $newUnit->addAlias('stone');
         $newUnit->addAlias('stones');
+        static::addUnit($newUnit);
+
+        $newUnit = UnitOfMeasure::linearUnitFactory('10 oz can', 4.5359237e-1 / 16 * 10);
+        $newUnit->addAlias('13');
+        static::addUnit($newUnit);
+
+        $newUnit = UnitOfMeasure::linearUnitFactory('13.5 oz can', 4.5359237e-1 / 16 * 13.5);
+        $newUnit->addAlias('14');
+        static::addUnit($newUnit);
+
+        $newUnit = UnitOfMeasure::linearUnitFactory('14.5 oz can', 4.5359237e-1 / 16 * 14.5);
+        $newUnit->addAlias('15');
+        static::addUnit($newUnit);
+
+        $newUnit = UnitOfMeasure::linearUnitFactory('15.5 oz can', 4.5359237e-1 / 16 * 15.5);
+        $newUnit->addAlias('16');
+        static::addUnit($newUnit);
+
+        $newUnit = UnitOfMeasure::linearUnitFactory('6 oz can', 4.5359237e-1 / 16 * 6);
+        $newUnit->addAlias('25');
+        static::addUnit($newUnit);
+
+        $newUnit = UnitOfMeasure::linearUnitFactory('14 oz can', 4.5359237e-1 / 16 * 14);
+        $newUnit->addAlias('26');
+        static::addUnit($newUnit);
+
+        $newUnit = UnitOfMeasure::linearUnitFactory('7 oz can', 4.5359237e-1 / 16 * 7);
+        $newUnit->addAlias('27');
+        static::addUnit($newUnit);
+
+        $newUnit = UnitOfMeasure::linearUnitFactory('8 oz can', 4.5359237e-1 / 16 * 8);
+        $newUnit->addAlias('33');
         static::addUnit($newUnit);
     }
 }
